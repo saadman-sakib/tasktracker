7bc1210e095a90609794c16b1e6f937f21d87e0d
Saadman Sakib <khubsadman@gmail.com>
2021-06-11 22:54:41 +0600

    messages   added

--- users/views.py
+++ users/views.py
@@ -3,12 +3,15 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import SignUpForm
 from django.contrib.auth import logout
+from django.contrib.messages.views import SuccessMessageMixin
 
 # Sign Up View
-class SignUpView(CreateView):
+class SignUpView(SuccessMessageMixin, CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'users/signup.html'
+    success_message = 'User Created!!!'
+    
 
 
 def logoutPage(request):
